46b92729094526c08bb650a92fc028c5a417d0f5
iris-zjdev <zj_shi@outlook.com>
2025-04-02 06:17:31 -0400

    session controller

--- app/models/meal_post.rb
+++ app/models/meal_post.rb
@@ -1,3 +1,15 @@
 class MealPost < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :bookmarks, dependent: :destroy
+
+  # Active Storage
+  has_one_attached :image
+
+  enum meal_type: { breakfast: 0, lunch: 1, dinner: 2 }
+  enum source_type: { homemade: 0, eating_out: 1 }
+
+  validates :title, presence: true
+  validates :content, presence: true
 end
